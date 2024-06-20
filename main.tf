2ec0b93df7a45fedf5fcd71ccef645ab2eab6c92
Derrick Ho <92569402+Dicksonkfk@users.noreply.github.com>
2024-06-20 19:44:30 +0800

    Updated S3 Access Control Lists for granting access to certain user

--- main.tf
+++ main.tf
@@ -20,23 +20,42 @@ resource "aws_s3_bucket_ownership_controls" "this" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "this" {
-  bucket = aws_s3_bucket.this.id
+#resource "aws_s3_bucket_public_access_block" "this" {
+#  bucket = aws_s3_bucket.this.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+#  block_public_acls       = false
+#  block_public_policy     = false
+#  ignore_public_acls      = false
+#  restrict_public_buckets = false
+#}
 
 resource "aws_s3_bucket_acl" "example" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.this,
-    aws_s3_bucket_public_access_block.this,
+    aws_s3_bucket_ownership_controls.this
   ]
-
   bucket = aws_s3_bucket.this.id
-  acl    = "public-read"
+
+  access_control_policy {
+    grant {
+      grantee {
+        id   = data.aws_authorized_s3_user.s3_owner.id
+        type = "CanonicalUser"
+      }
+      permission = "READ"
+    }
+
+    grant {
+      grantee {
+        type = "Group"
+        uri  = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+      }
+      permission = "READ_ACP"
+    }
+
+    owner {
+      id = data.aws_authorized_s3_user.s3_owner.id
+    }
+  }
 }
 
 resource "aws_s3_bucket_policy" "this" {
@@ -63,3 +82,5 @@ resource "aws_s3_bucket_policy" "this" {
 data "aws_s3_objects" "this" {
   bucket = aws_s3_bucket.this.bucket
 }
+
+data "aws_authorized_s3_user" "s3_owner"{}
